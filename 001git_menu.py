d85fa7d90d7568f7d488f1267e113e6c2bd3e338
Juliet Amorim <julietamorim06@gmail.com>
2022-07-29 19:51:28 -0300

    update in all input's

--- 001git_menu.py
+++ 001git_menu.py
@@ -9,27 +9,27 @@ while opcao != 5:
    [ 3 ] NUMERO MAIOR
    [ 4 ] NOVOS NÚMEROS
    [ 5 ] SAIR DO PROGRAMA''')
-   opcao = int(input('>>>>> Qual a sua opção? '))
+   opcao = int(input('>>>>> Escolha uma opção do nosso menu: '))
    if opcao == 1:
        soma = num1 + num2
-       print(f'A soma entre {num1} + {num2} é {soma}')
+       print(f'A soma entre os dois números, {num1} e {num2}, é {soma}')
    elif opcao == 2:
        multi = num1 * num2
-       print(f'A multiplicação entre {num1} x {num2} é {multi}')
+       print(f'A multiplicação entre os dois números, {num1} e {num2}, é {multi}')
    elif opcao == 3:
        if num1 > num2:
            maior = num1
        else:
            maior = num2
-       print(f'Entre {num1} e {num2} o maior número é {maior}')
+       print(f'Entre os dois números, {num1} e {num2}, o maior número é {maior}')
    elif opcao == 4:
-       print('informe os números novamente: ')
+       print('Por favor, informe os números novamente: ')
        num1 = float(input('Digite o 1º número: '))
        num2 = float(input('Digite o 2º número: '))
    elif opcao == 5:
-       print('Finalizando...')
+       print('Finalizando o programa...')
    else:
-       print('Opção inválida. Tente novamente!')
+       print('Não temos essa opção. Tente novamente!')
    print('=-=' * 10)
    sleep(2)
 print('Fim do programa! Volte sempre!')
