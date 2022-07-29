c2440aff3a7a2632c3f122c3b7687e4fdd9b01af
Juliet Amorim <julietamorim06@gmail.com>
2022-07-29 16:56:46 -0300

    Update name menu [3]

--- 001git_menu.py
+++ 001git_menu.py
@@ -6,7 +6,7 @@ while opcao != 5:
    print('''
    [ 1 ] SOMAR
    [ 2 ] MULTIPLICAR
-   [ 3 ] MAIOR
+   [ 3 ] NUMERO MAIOR
    [ 4 ] NOVOS NÚMEROS
    [ 5 ] SAIR DO PROGRAMA''')
    opcao = int(input('>>>>> Qual a sua opção? '))
